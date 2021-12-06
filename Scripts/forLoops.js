b2526ba23c249d1618623690581090f6d9879d4d
NivedithaSekar <sekarniveditha@gmail.com>
2021-12-06 23:59:44 +0530

    initial commit

---
+++ Scripts/forLoops.js
@@ -0,0 +1,68 @@
+import resume from "/Scripts/resume.js" ;
+
+
+//for loop - properties/index value is considered
+var forloop = function(data,properties){
+    for(let i=0; i< properties.length; i++){
+        //if data is not an array object / array of some objects in it -> Looping again to destructure them
+        //else print the given data
+        if(data[properties[i]] instanceof Object && !(data[properties[i]] instanceof Array) || (data[properties[i]] instanceof Array && (data[properties[i]].some((value) => {return typeof value === 'object'})))){
+            forloop(data[properties[i]],Object.keys(data[properties[i]]));
+        }
+        else{
+            console.log(`${properties[i]} : ${data[properties[i]]}`)
+        }
+    }
+}
+forloop(resume,Object.keys(resume));
+console.log("*******************************************************");
+
+//for in
+/**
+ * The for...in statement iterates over the enumerable properties of an object. 
+ *      enumerable property - properties which are created by simple assignment or property initializer are enumerable by default
+ *      non-enumerable property -  Properties that are created using the defineProperty() method have enumerable flag set to false usually. this makes the property invisible while printing
+ */
+var forin = function(data){
+    for(const key in data){
+        if(data[key] instanceof Object && !(data[key] instanceof Array) || (data[key] instanceof Array && (data[key].some((value) => {return typeof value === 'object'})))){
+            forin(data[key]);
+        }
+        else{
+            console.log(`${key} : ${data[key]}`)
+        }
+    }
+}
+forin(resume);
+console.log("*******************************************************");
+
+//for of
+/**
+ * The for...of statement iterates over values that the iterable object defines to be iterated over.  
+ */
+var forof = function(resume){
+    for(const key of Object.keys(resume)){
+        if(resume[key] instanceof Object && !(resume[key] instanceof Array) || (resume[key] instanceof Array && (resume[key].some((value) => {return typeof value === 'object'})))){
+            forof(resume[key]);
+        }
+        else{
+            console.log(`${key} : ${resume[key]}`)
+        }
+    }
+}
+forof(resume);
+console.log("*******************************************************");
+//foreach
+//console.log(Object.entries(resume));
+var foreach = function(resume){
+    Object.entries(resume).forEach(([key, value]) => {
+        if(resume[key] instanceof Object && !(resume[key] instanceof Array) || (resume[key] instanceof Array && (resume[key].some((value) => {return typeof value === 'object'})))){
+            foreach(resume[key]);
+        }
+        else{
+            console.log(`${key} : ${value}`)
+        }
+        
+    });
+}
+foreach(resume);
---
+++ Scripts/resume.js
@@ -0,0 +1,59 @@
+export default {
+    "basic":{
+        "name":"Niveditha",
+        "designation": "Engineer",
+        "contactDetails": {
+            "email":"niveditha@gmail.com",
+            "phone":"+91-9786534210",
+            "address":{
+                "houseNumber": "No 123",
+                "street": "24th Houston street",
+                "area": "Shanthi colony, Anna nagar",
+                "city": "Chennai",
+                "pincode": "600002",
+                "state": "TamilNadu",
+                "country": "India"
+            }
+        },
+        "url":"https://nivedithasekar.github.io/HTML-CSS/",
+        "profileSummary":"To be innovative and resourceful in a challenging work environment with profound interest in Artificial Intelligence and Web Development. Looking for an opportunity, where I can implement, learn and enhance my skills for the organization.",
+        "languagesKnown":["tamil","english","hindi"]
+    },
+    "softSkills":["Teamwork","Leadership","Adaptability","Fast Learner","Communication Skills","Problem Solving"],
+    "technicalSkills":{
+        "Applications": ["VSCode","Eclipse","Git","Microsoft Office"],
+        "languages": ["HTML","CSS","JavaScript","Python","Java"]
+    },
+    "hobbies":["Sketching","Reading"],
+    "employementHistory":[
+        {
+            "company": "Virtusa",
+            "designation":"Engineer-Technology",
+            "startDate":"OCTOBER 2019",
+            "endDate":"PRESENT",
+            "domain":"IT-Service Management",
+            "highlights":["Managed and tracked the delivery of CR's/SRT's affecting the dropped and defaulted records of KPI",
+                "Refreshed and Updated PCAT - Power BI dashboard",
+                "Analyzed Risk and Controls data and CHGs of the Applications",
+                "Prepared User manuals, Presentations for weekly call"
+                ]
+        }
+    ],
+    "education":[
+        {
+            "board":"University",
+            "name":"RMK Engineering College",
+            "percentage":"75%"
+        },
+        {
+            "board":"HSC",
+            "name":"Kurinji Educational Trust",
+            "percentage":"94%"
+        },
+        {
+            "board":"SSLC",
+            "name":"St. Charles Hr. Sec School",
+            "percentage":"98%"
+        }
+    ]
+}
